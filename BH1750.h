2b6acc4168bf0522e8a159e553e07b2ed14a331b
Deltes23 <to.lu@wp.pl>
2020-02-09 12:33:31 +0100

    Add files via upload
    dodana dokumentacja doxygen

--- BH1750.h
+++ BH1750.h
@@ -1,3 +1,9 @@
+/**
+  * @file           BH1750.h
+  * @brief          Ten plik zawiera definicje funkcji obsługi czujnika BH1750
+  * @author			Nieznany
+  */
+
 #ifndef INC_BM1750_H_
 #define INC_BM1750_H_
 
@@ -12,27 +18,64 @@
 #define BH1750_CONVERSION_FACTOR	1.2
 
 typedef enum {
-	BH1750_OK		= 0,
-	BH1750_ERROR	= 1
+BH1750_OK		= 0,
+BH1750_ERROR	= 1
 } BH1750_STATUS;
 
 typedef enum
 {
-    CONTINUOUS_HIGH_RES_MODE  	= 0x10,
-    CONTINUOUS_HIGH_RES_MODE_2 	= 0x11,
-    CONTINUOUS_LOW_RES_MODE 	= 0x13,
-    ONETIME_HIGH_RES_MODE 		= 0x20,
-    ONETIME_HIGH_RES_MODE_2 	= 0x21,
-    ONETIME_LOW_RES_MODE 		= 0x23
+    CONTINUOUS_HIGH_RES_MODE  	= 0x10, /*!< wysoka rozdzielczość w trybie ciągłym */
+    CONTINUOUS_HIGH_RES_MODE_2 	= 0x11, /*!< wysoka rozdzielczość w trybie ciągłym 2 */
+    CONTINUOUS_LOW_RES_MODE 	= 0x13, /*!< niska rozdzielczość w trybie ciągłym */
+    ONETIME_HIGH_RES_MODE 		= 0x20, /*!< wysoka rozdzielczość w trybie pojedynczego odczytu */
+    ONETIME_HIGH_RES_MODE_2 	= 0x21, /*!< wysoka rozdzielczość w trybie pojedynczego odczytu 2 */
+    ONETIME_LOW_RES_MODE 		= 0x23 /*!< niska rozdzielczość w trybie pojedynczego odczytu */
 }bh1750_mode;
 
+/*! \enum bh1750_mode
+ * Zawiera zdefiniowane wartości służące do wyboru trybu pracy czujnika
+ */
+
 BH1750_STATUS BH1750_init(I2C_HandleTypeDef *hi2c);
+/*! \fn BH1750_STATUS BH1750_init(I2C_HandleTypeDef *hi2c)
+ *  \brief Funkcja inicjująca pracę czujnika
+ *  \param hi2c wybór oprawy HAL i2c
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_reset(void);
+/*! \fn BH1750_STATUS BH1750_reset(void)
+ *  \brief Funkcja resetująca czujnik
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_power_state(uint8_t power_on);
+/*! \fn BH1750_STATUS BH1750_power_state(uint8_t power_on)
+ *  \brief Włączenie/wyłączenie czujnika
+ *  \param power_on 1 żeby uruchomić, 0 żeby wyłączyć
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_set_mtreg(uint8_t Mtreg);
+/*! \fn BH1750_STATUS BH1750_set_mtreg(uint8_t Mtreg)
+ *  \brief Ustawienie rejestru measurement time, służącego do zmiany okresu pomiaru
+ *  \param Mtreg przyjmuje wartość od 0001_1111 do 1111_1110
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_set_mode(bh1750_mode mode);
+/*! \fn BH1750_STATUS BH1750_set_mode(bh1750_mode mode)
+ *  \brief Zmiana trybu pracy czujnika
+ *  \param mode wybór trybu
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_trigger_manual_conversion(void);
+/*! \fn BH1750_STATUS BH1750_trigger_manual_conversion(void)
+ *  \brief Wyzwalanie ręcznej konwersji
+ *  \return BH1750_STATUS
+ */
 BH1750_STATUS BH1750_read(float *result);
+/*! \fn BH1750_STATUS BH1750_read(float *result)
+ *  \brief Funkcja odczytywania wartości natężenia światła
+ *  \param result zmiennoprzycinkowa wartość zawierająca wynik, przekazywana przez referencję
+ *  \return BH1750_STATUS
+ */
 
 
 #endif /* INC_BM1750_H_ */
